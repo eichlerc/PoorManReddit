cdd8282f8c451d9c370bfd035931681d7167f9d4
Chelsey Eichler <chelsey.eichler@yahoo.com>
2017-03-01 20:13:46 -0500

    got the sections to link

--- script.js
+++ script.js
@@ -6,19 +6,20 @@ function(goodness){
 
     for(var i = 0 ; i<redditArray.length ; i++){
     var thumbnail = redditArray[i].data.thumbnail;
+    var permalink = "http://www.reddit.com" + redditArray[i].data.permalink;
     var title = redditArray[i].data.title;
     var author = redditArray[i].data.author;
     var score = redditArray[i].data.score;
 
     $('section').append(
-        '<div><h2>' + title + '<br>' + author + '<br>' + score +
+        '<div><a href = "'  + permalink + '"> <h2>' + title + '<br>' + author + '<br>' + score +
         '</h2><img src="'
         + thumbnail +
-        '"/></div>'
+        '"/></a></div>'
       );
-    }
+
+}
 
   });
-
 
 });
