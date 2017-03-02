fc1397f472a3bdffa99f0b47c8be5f040db0902c
Chelsey Eichler <chelsey.eichler@yahoo.com>
2017-03-01 19:25:09 -0500

    got posts to show up with a little styling

--- script.js
+++ script.js
@@ -7,13 +7,15 @@ function(goodness){
     for(var i = 0 ; i<redditArray.length ; i++){
     var thumbnail = redditArray[i].data.thumbnail;
     var title = redditArray[i].data.title;
+    var author = redditArray[i].data.author;
     var score = redditArray[i].data.score;
 
     $('section').append(
-        '<div><h2>' + title +
+        '<div><h2>' + title + '<br>' + author + '<br>' + score +
         '</h2><img src="'
-        + thumbnail+
-        '"/></div>');
+        + thumbnail +
+        '"/></div>'
+      );
     }
 
   });
